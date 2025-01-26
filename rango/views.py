0fed06d477cc63bcba38da487482c0419c46bba5
Xuechun Ji <luandun1018@gmail.com>
2025-01-26 13:53:08 +0000

    Chapter 3 exercised completed

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return <a href="https://tango_with_django_project/">Vist Project</a> ("Rango says hey there partner!")
+
+def about(request):
+    return <a href="https://tango_with_django_project/about/">Vist More Info</a>("Rango says here is the about page.")
